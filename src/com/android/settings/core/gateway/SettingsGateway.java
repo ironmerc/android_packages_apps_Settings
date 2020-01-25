AquariOS/packages_apps_Settings

Long press custom header to open custom header settings [2/2]
Change-Id: I5ce74197ef3a980a937811f8c0fe2d8d2d5020c8

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.aquarios.coralreef.CoralReef;
+import com.aquarios.coralreef.fragments.CustomHeaders;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 public class SettingsGateway {
@@ -274,7 +275,8 @@ public class SettingsGateway {
             SmartbarSettings.class.getName(),
             FlingSettings.class.getName(),
             PulseSettings.class.getName(),
-            SwipeUpGestureSettings.class.getName()
+            SwipeUpGestureSettings.class.getName(),
+            CustomHeaders.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -318,6 +320,7 @@ public class SettingsGateway {
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(), 
             Settings.PulseSettingsActivity.class.getName(),
-            Settings.StockNavigationActivity.class.getName()
+            Settings.StockNavigationActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }

ironmerc/android_packages_apps_Settings

Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -151,6 +151,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.dirtyunicorns.tweaks.DirtyTweaks;
 import com.dirtyunicorns.tweaks.fragments.GamingMode;
+import com.dirtyunicorns.tweaks.fragments.QsHeader;
 
 public class SettingsGateway {
 
@@ -290,7 +291,8 @@ public class SettingsGateway {
             DirtyTweaks.class.getName(),
             StartThemes.class.getName(),
             GamingMode.class.getName(),
-            RunningServices.class.getName()
+            RunningServices.class.getName(),
+            QsHeader.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -332,5 +334,6 @@ public class SettingsGateway {
             Settings.DirtyTweaksActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }