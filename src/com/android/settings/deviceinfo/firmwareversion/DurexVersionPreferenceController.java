c63a668ec3cf116ee50218afd4174efdcb698598
ironmerc <sinhaakshat2012@gmail.com>
2020-02-04 16:24:11 +0000

    Revert "No more durex"
    This reverts commit e98fc2a5f18749ba25510e397d618248998880dd.

--- src/com/android/settings/deviceinfo/firmwareversion/DurexVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/DurexVersionPreferenceController.java
@@ -30,14 +30,14 @@ import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
-public class CrvsVersionPreferenceController extends BasePreferenceController {
+public class DurexVersionPreferenceController extends BasePreferenceController {
 
-    private static final Uri INTENT_URI_DATA = Uri.parse("https://github.com/Corvus-OS/");
-    private static final String TAG = "crvsDialogCtrl";
-    private static final String CRVS_VERSION_PROPERTY = "ro.du.version";
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://github.com/du-rex/");
+    private static final String TAG = "durexDialogCtrl";
+    private static final String DUREX_VERSION_PROPERTY = "ro.du.version";
     private final PackageManager mPackageManager = this.mContext.getPackageManager();
 
-    public CrvsVersionPreferenceController(Context context, String preferenceKey) {
+    public DurexVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
     }
 
@@ -46,9 +46,9 @@ public class CrvsVersionPreferenceController extends BasePreferenceController {
     }
 
     public CharSequence getSummary() {
-        String crvs = SystemProperties.get(CRVS_VERSION_PROPERTY,
+        String durex = SystemProperties.get(DUREX_VERSION_PROPERTY,
                 mContext.getString(R.string.device_info_default));
-        return crvs;
+        return durex;
     }
 
     public boolean handlePreferenceTreeClick(Preference preference) {
